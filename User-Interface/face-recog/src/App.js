1a5f45a30f602f60fc6afd9749e6220663f13476
udit <tudrishu@gmail.com>
2024-09-22 02:12:59 +0530

    react ui changes

--- User-Interface/face-recog/src/App.js
+++ User-Interface/face-recog/src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
+
 import './App.css';
+import Header from './components/Header';
+import About from './pages/About';
+import Attendance from './pages/Attendance';
+import Register from './pages/Register';
+import { Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-
+      <Route path="./" component={Header} />
+      <Route path='/pages/About' component={About} />
     </>
   );
 }
